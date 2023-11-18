55e52da57550bceb0d6a767a69ff00a3c7d0bd1f
nicolashoeller <hoellr3.0@gmail.com>
2023-11-18 14:20:25 +0100

    loop for better experience

--- numberGuessing.asm
+++ numberGuessing.asm
@@ -2,8 +2,8 @@
 
 .data
 	my_string: .asciiz "Guess the number between 0 and 20\n"
-    	my_string_false: .asciiz "False, try again!\n"
-    	my_string_right: .asciiz "You'r right, 10 is the right number\n"
+    	my_string_false: .asciiz "False, try again!\n\n"
+    	my_string_right: .asciiz "You'r right, 7 is the right number!!\n"
 
 .text
 	main:
@@ -16,18 +16,14 @@
 	syscall
         move $t0, $v0   		        # Move input to $t0
 
-        beq $t0, 10, _right_input	    	# If $t0 eq 10 go to _right_input
+        beq $t0, 7, _right_input	    	# If $t0 eq 10 go to _right_input
 
         la $a0, my_string_false		    	# Load my_string_false in $a0
         li $v0, 4
         syscall
-        j _ende
+        j main
 
 _right_input:
 	la $a0, my_string_right         	# Load my_string_right in $a0
         li $v0, 4
         syscall
-
-_ende:
-	li $v0, 10
-	syscall
