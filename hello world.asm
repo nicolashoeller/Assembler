667f9e34954c226a40c9273f66895494f35ba8e9
nicolashoeller <hoellr3.0@gmail.com>
2023-11-18 23:07:38 +0100

    hello world program

--- hello world.asm
+++ hello world.asm
@@ -2,10 +2,15 @@
 
 .data 
 	hello_world: .asciiz "Hello World!\n"
+	fuck_the_world: .asciiz "I'm gonna fuck the you\n"
 	
 .text
 	main:
 	
 	la $a0, hello_world
+	li $v0, 4
+	syscall
+	
+	la $a0, fuck_the_world
 	li $v0, 4
 	syscall
