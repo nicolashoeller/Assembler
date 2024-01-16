350cc53f1cf3a7ad72d99d69eb49d9ea8dad3622
nicolashoeller <hoellr3.0@gmail.com>
2024-01-16 10:29:30 +0100

    Number guessing game with random numbers

--- numberGuessing.asm
+++ numberGuessing.asm
@@ -3,11 +3,16 @@
 .data
 my_string: .asciiz "Guess the number between 0 and 20\n"
 my_string_false: .asciiz "False, try again!\n\n"
-my_string_right: .asciiz "You'r right, 7 is the right number!!\n"
+my_string_right: .asciiz "You'r right, that is the right number!!\n"
 
 .text
 main:
+	li $v0, 41				# Generate random number to $a0
+	move $t1, $a0
+	syscall
 	
+	remu $t1, $t1, 20
+game:
 	la $a0, my_string
 	li $v0, 4       		        # Syscall for printf()
 	syscall
@@ -15,13 +20,13 @@ main:
 	li $v0, 5
 	syscall
         move $t0, $v0   		        # Move input to $t0
-
-        beq $t0, 7, _right_input	    	# If $t0 eq 10 go to _right_input
+	
+        beq $t0, $t1, _right_input	    	# If $t0 eq $t1 go to _right_input
 
         la $a0, my_string_false		    	# Load my_string_false in $a0
         li $v0, 4
         syscall
-        j main
+        j game
 
 _right_input:
 	la $a0, my_string_right         	# Load my_string_right in $a0
